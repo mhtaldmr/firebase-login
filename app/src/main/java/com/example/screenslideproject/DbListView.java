cbc11dac2b6892ab7dfab2fbd9e8bc374e0ec99e
mhtaldmr <mht.alidemir@gmail.com>
2019-12-09 09:43:42 +0300

    firebase

---
+++ app/src/main/java/com/example/screenslideproject/DbListView.java
@@ -0,0 +1,14 @@
+package com.example.screenslideproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DbListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db_list_view);
+    }
+}
---
+++ app/src/main/java/com/example/screenslideproject/MainActivity.java
@@ -0,0 +1,205 @@
+package com.example.screenslideproject;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int GOOGLE_SIGN = 123;
+    Button btn_login = null;
+    Button btn_logout = null;
+    Button btn_enter = null;
+    ImageView image = null;
+    TextView text = null;
+    ProgressBar progressBar = null;
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.login);
+        btn_logout = findViewById(R.id.logout);
+        btn_enter = findViewById(R.id.enter);
+        text = findViewById(R.id.text);
+        image = findViewById(R.id.image);
+        progressBar = findViewById(R.id.progress_circular);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
+
+        btn_login.setOnClickListener(v -> SingInGoogle());
+        btn_logout.setOnClickListener(v -> Logout());
+
+        if (mAuth.getCurrentUser() != null){
+            FirebaseUser user = mAuth.getCurrentUser();
+            updateUI(user);
+        }
+    }
+
+
+    void SingInGoogle() {
+        progressBar.setVisibility(View.VISIBLE);
+        Intent i = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(i,GOOGLE_SIGN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( requestCode == GOOGLE_SIGN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account != null){
+                    firebaseAuthWithGoogle(account);
+                }
+            } catch (ApiException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d("TAG","firebaseAuthWithGoogle: "+ account.getId());
+        AuthCredential authCredential = GoogleAuthProvider
+                .getCredential(account.getIdToken(), null);
+
+        mAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()){
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Log.d("Tag","SignIn success");
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    }
+                    else{
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Log.w("Tag","SignIn failure",task.getException());
+
+                        Toast.makeText(this, "SignIn Failed", Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                    }
+                });
+
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null ){
+
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String photo = String.valueOf(user.getPhotoUrl());
+
+            text.append(" info: \n");
+          //  text.append(name+"\n");
+            text.append(email);
+
+           // Picasso.get().load(photo).into(image);
+            btn_login.setVisibility(View.INVISIBLE);
+            btn_logout.setVisibility(View.VISIBLE);
+            btn_enter.setVisibility(View.VISIBLE);
+            btn_enter.setOnClickListener(view -> {
+                Intent i = new Intent(getApplicationContext(),DbListView.class);
+                startActivity(i);
+            });
+        }
+        else {
+            text.setText("Firebase Login");
+          //  Picasso.get().load(R.drawable.ic_firebase_logo).into(image);
+            btn_login.setVisibility(View.VISIBLE);
+            btn_logout.setVisibility(View.INVISIBLE);
+            btn_enter.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    void Logout(){
+        FirebaseAuth.getInstance().signOut();
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this,task -> updateUI(null));
+
+    }
+
+
+
+
+
+
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_ayarlar:
+                Intent i = new Intent(Settings.ACTION_SETTINGS);
+                startActivityForResult(i,0);
+                return true;
+            case R.id.action_arama:
+                Uri number = Uri.parse("tel:5551234");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                startActivity(callIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+}
+
+ */
+
+
+
+}
